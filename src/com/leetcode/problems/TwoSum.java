b3277daeb2bd68bf0ed9c287bc5cef2b4c8ca5cf
Vijeyakumar <65109903+Vijeyakumar26@users.noreply.github.com>
2021-03-06 21:46:18 +0530

    Initial commit

---
+++ src/com/leetcode/problems/TwoSum.java
@@ -0,0 +1,14 @@
+package com.leetcode.problems;
+
+public class TwoSum {
+	public static void main(String[] args) {
+		int[] nums = {2,3,6};
+		int target = 8;
+		int[] result = twoSum(nums, target);
+	}
+	public static int[] twoSum(int[] nums, int target) {
+	
+		
+		return nums;
+	}
+}
